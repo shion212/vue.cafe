dbb3e34ff19e19028d32f814461e0945b33725d2
イワムラシオン <68310617+shion212@users.noreply.github.com>
2021-03-02 00:09:26 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+$(function(){
+    let $window = $(window),
+        $load = $('#load_animation'),
+        $load_in = $load.find('img');
+
+    //ロゴとLoadingGIFをフェードイン
+    $load_in.delay(500).fadeIn('slow');
+
+    //ウィンドウに対してloadイベントを設定
+    //全ての要素をロードが終わったら，HideLoadingScreen()を実行する
+    $window.on('load',function(){
+        HideLoadingScreen();
+    })
+    
+    //５秒後に強制的にHideLoadingScreen()を実行
+    setTimeout(function(){
+        HideLoadingScreen();
+    },5000);
+
+    //ロード画面を非表示にする関数
+    function HideLoadingScreen(){
+        $load.delay(5000).fadeOut("slow");
+    }
+}); 
+
+
+
+
+
+
+
+
+  
+
